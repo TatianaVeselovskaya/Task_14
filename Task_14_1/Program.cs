e78e515c631b001b90f6922645a46d520626a18c
Татьяна <veselovskayt@mail.ru>
2021-12-26 15:52:17 +0700

    Добавьте файлы проекта.

---
+++ Task_14_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Феликс", "Cибирская кошка", "мяу");
+            cat.ShowInfo();
+            Dog dog = new Dog("Шарик", "гав");
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_14_1/say.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+
+    class Cat : Animal
+    {
+        string breed;
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Cat(string name, string breed, string say)
+            : base(name)
+        {
+            this.breed = breed;
+        }
+        public override void Say()
+        {
+            Console.WriteLine(breed);
+            Console.WriteLine("Мяу");
+            Console.WriteLine();
+        }
+    }
+    class Dog : Animal
+    {
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name, string say)
+            : base(name)
+        {
+           
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
